4915173edf48b397fe4b6dd3e99e71e36acf6dc4
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-12-05 12:23:19 +0000

    Tasks

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -13,6 +13,19 @@
  * Description: singly linked list node structure
  * for ALX projects
  */
+def print_list_integer(my_list=[]);
+def element_at(my_list, idx);
+def replace_in_list(my_list, idx, element);
+def print_reversed_list_integer(my_list=[]);
+def new_in_list(my_list, idx, element);
+def no_c(my_string);
+def print_matrix_integer(matrix=[[]]);
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+
 typedef struct listint_s
 {
 	int n;
@@ -25,5 +38,5 @@ void free_listint(listint_t *head);
 
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
-
+void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
