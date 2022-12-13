28f6a05d881051a11b687a4fb004a938060874ef
“TijesuRejoice” <tijesuadeleke94gmail.com>
2022-12-13 09:57:56 +0000

    Task 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - TijesuRejoice
+
+def magic_calculator(a, b, c):
+    """Match bytecode provided by ALX School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
