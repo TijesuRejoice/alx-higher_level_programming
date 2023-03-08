6c4d0dd903dfa12d1773a9c45b2e740fe3464c3d
“TijesuRejoice” <tijesuadeleke94gmail.com>
2023-03-08 11:16:03 +0000

    zero

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """Defines a text file-reading function."""
 
 
-def reaed_file(filename=""):
+def read_file(filename=""):
     """Print the content of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
