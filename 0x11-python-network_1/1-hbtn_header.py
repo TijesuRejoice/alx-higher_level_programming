b87b924950657b3ef5b6a0896f8f866d21986164
“TijesuRejoice” <tijesuadeleke94gmail.com>
2023-04-03 13:34:45 +0000

    Task one

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header of the response.
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./1-hbtn_header.py <URL>
 """
 import sys
 import urlib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
