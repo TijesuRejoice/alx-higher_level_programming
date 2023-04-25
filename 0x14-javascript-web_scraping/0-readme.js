f6d464d31ae913917174f2bff804ae85104a9c4a
“TijesuRejoice” <tijesuadeleke94gmail.com>
2023-04-25 11:57:48 +0000

    Task zero

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filepath = process.argv[2];
-
-fs.readfile(filePath, 'utf8', function(err, data) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	console.log(error || content);
 });
 
